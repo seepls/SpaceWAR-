f0c5324962b10b226516bef5429ff9dd4a8a2024
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-22 15:05:26 +0530

    Create SpacePanel.java

---
+++ SpacePanel.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+
+public class SpacePanel extends JPanel impliments Runnable , KeyListener {
+public final static int width = 800 ;
+public final static int height = 600 ;
+private boolean running,  waveStart, keyControl= false;
+private BufferedImage image ,imgPlayer , bk , background1 ,background2 ,background3,background4,background5,background6,background7,background8;
+private double averageFps ;
+private Graphics2D g ;
+private int fps = 30, waveDelay = 2000, limitLevel = 36;
+
+
+}
+
+
